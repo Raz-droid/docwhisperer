ade535cb18d0df75920a65035156842e661a5392
Raz-droid <your-email@example.com>
2025-04-13 16:54:21 +0530

    first commit need to migrate schema now

---
+++ src/lib/db/schema.ts
@@ -0,0 +1,22 @@
+
+
+import { pgEnum, pgTable, serial, text, timestamp, varchar } from "drizzle-orm/pg-core"
+export const userSystemEnum = pgEnum("userSystemENum",["user",'system'])
+
+export const Chats = pgTable("Chats",{
+    chat_id : serial("chat_id").primaryKey(),
+    pdf_name : text("pdf_name").notNull(),
+    pdf_url : text("pdf_url").notNull(),
+    created_at : timestamp("created_at").notNull().defaultNow(),
+    user_id : varchar("user_id",{length:256}).notNull(),
+    file_key : text("file_key").notNull()
+
+}) 
+
+export const messages = pgTable("messages",{
+    message_id : serial("message_id").primaryKey(),
+    chat_id : text("chat_id").references(()=>Chats.chat_id),
+    content : text("content").notNull(),
+    created_at : timestamp("created_at").notNull().defaultNow(),
+    role : userSystemEnum("role").notNull()
+})
