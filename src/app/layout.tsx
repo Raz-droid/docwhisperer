361e9367e7423e6160d3d54fb2a18ec40bae1ca8
Raz-droid <your-email@example.com>
2025-04-17 11:25:43 +0530

    solved the headeriissue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,24 @@ export default function RootLayout({
       <Providers>
       <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-  
+        <SignedOut>
+        <header className="flex justify-end items-center p-4 gap-4 h-16">
+          
+          
+                <SignUpButton mode="modal">
+                  <div>
+                    <Button asChild className="w-20 h-10 hover:bg-teal-600 transition cursor-pointer">
+                      <span className="flex items-center justify-center">
+                        Sign Up
+                        
+                      </span>
+                    </Button>
+                  </div>
+                </SignUpButton>
+          
+          
+        </header>
+        </SignedOut>
         {children}
         <Toaster/>
       </body>
